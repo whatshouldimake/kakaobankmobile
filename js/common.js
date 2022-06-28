4d4aa613d1fca603db99b86920bcb51e459217e9
whatshouldimake <wndud6952@gmail.com>
2022-06-28 23:12:05 +0900

    기존작업모든파일업로드

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(function () {
+    //전체메뉴 보기 클릭
+    $(".btn_show_all_menu").click(function () {
+      $(".all_menu").show();
+    });
+    //전체메뉴 닫기 클릭
+    $(".btn_hide_all_menu").click(function () {
+      $(".all_menu").hide();
+    });
+  });
+  
